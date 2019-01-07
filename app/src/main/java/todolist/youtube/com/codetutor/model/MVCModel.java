51af322d4d02f83ddcbd00ac2ac8c1e1d3f4d6de
Anil Deshpande <anildesh82@gmail.com>
2019-01-07 12:24:11 +0530

    incomplete code 2

--- app/src/main/java/todolist/youtube/com/codetutor/model/MVCModel.java
+++ app/src/main/java/todolist/youtube/com/codetutor/model/MVCModel.java
@@ -3,6 +3,7 @@ package todolist.youtube.com.codetutor.model;
 import todolist.youtube.com.codetutor.model.bean.ToDo;
 
 public interface MVCModel {
+
     public boolean addToDoItem(String toDoItem, String place);
     public boolean removeToDoItem(int id);
     public boolean modifyToDoItem(int id, String newToDoValuel);
