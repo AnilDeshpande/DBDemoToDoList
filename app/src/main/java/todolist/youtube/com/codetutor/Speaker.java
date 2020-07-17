07d78b67ac2e6ee4213ab6ba7f99d3d681fc7428
Anil Deshpande <anildesh82@gmail.com>
2020-07-17 16:50:42 +0530

    Adding a documentation

--- app/src/main/java/todolist/youtube/com/codetutor/Speaker.java
+++ app/src/main/java/todolist/youtube/com/codetutor/Speaker.java
@@ -2,6 +2,11 @@ package todolist.youtube.com.codetutor;
 
 import todolist.youtube.com.codetutor.bean.ToDo;
 
+/**
+ * Any View which needs to do the speaking part
+ * needs to implement this interface. In this case MainActivity
+ * is implementing this interface and override this method
+ */
 public interface Speaker {
     void speak(ToDo toDo);
 }
